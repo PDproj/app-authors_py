06cef8094076bdc988ac45cbfe4f314b301644cd
PDproj <126194183+PDproj@users.noreply.github.com>
2023-02-28 21:24:42 -0500

    Corrected errors

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import uvicorn
 import os
 
 # cast to int
-SERVER_PORT = int(os.environ.get('SERVER_PORT', 8000))
+SERVER_PORT = int(os.environ.get('SERVER_PORT', 7072))
 
 if __name__ == "__main__":
     uvicorn.run("server.app:app", host="0.0.0.0",
